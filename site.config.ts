399c8ef8c48f57ec0f9f5017ed0d736f7053a6ff
ictcentervnn <92720028+ictcentervnn@users.noreply.github.com>
2022-07-02 10:48:56 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -20,7 +20,7 @@ export default siteConfig({
   twitter: '#',
   github: 'ictcentervnn',
   linkedin: '#',
-  // newsletter: '#', // optional newsletter URL
+  newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
