444c8d45a599e92bfc2f7c31515d2db9de7948be
ictcentervnn <92720028+ictcentervnn@users.noreply.github.com>
2022-07-02 10:41:43 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'ictcentervnn',
+  twitter: '#',
   github: 'ictcentervnn',
-  linkedin: 'ictcentervnn',
+  linkedin: '#',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
